0a41d313f1c25515301d9d33b2d7c847a7e8ed69
Jake Dromgoole <tjd210@yahoo.com>
2015-06-09 11:01:46 -0500

    Whole project commit

---
+++ Capstone_2/SQL /SQL_Code.sql
@@ -0,0 +1,20 @@
+SET SCHEMA APP;
+
+CREATE TABLE Musicians 
+	(id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
+	name VARCHAR(40) NOT NULL, 
+	age INT NOT NULL,
+	location VARCHAR(40) NOT NULL,
+	instrument VARCHAR(40) NOT NULL,
+	experience INT NOT NULL
+	);
+	
+INSERT INTO MUSICIANS (name, age, location, instrument, experience)  values ('Slash', 25, 'Los Angeles', 'guitar', 5);
+INSERT INTO MUSICIANS (name, age, location, instrument, experience)  values ('Flea', 30, 'New York', 'bass', 10);
+
+
+update MUSICIANS set theYear = 2009, color = 'Green' where id = 2;
+
+select * from musicians;
+
+select * from musicians where instrument = 'guitar';
